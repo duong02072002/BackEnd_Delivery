bfc34c0a036ee1c66c64469989389bc056206dc2
duong02072002 <duong02072002.com>
2024-06-03 19:42:31 +0700

    10

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -157,8 +157,7 @@ module.exports = {
 
     },
     async registerDriverWithImage(req, res) {
-
-        const user = JSON.parse(req.body.user); // TÔI LẤY DỮ LIỆU KHÁCH HÀNG GỬI CHO TÔI
+        const user = JSON.parse(req.body.user); // Lấy dữ liệu mà người dùng gửi
 
         const files = req.files;
 
@@ -172,8 +171,6 @@ module.exports = {
         }
 
         User.create(user, (err, data) => {
-
-
             if (err) {
                 return res.status(501).json({
                     success: false,
@@ -186,9 +183,8 @@ module.exports = {
             const token = jwt.sign({ id: user.id, email: user.email }, keys.secretOrKey, {});
             user.session_token = `JWT ${token}`;
 
-
-            Rol.create(user.id, 2, (err, data) => {
-
+            // Tạo vai trò 2 (delivery)
+            Rol.create(user.id, 2, async (err, data) => {
                 if (err) {
                     return res.status(501).json({
                         success: false,
@@ -197,17 +193,24 @@ module.exports = {
                     });
                 }
 
-                return res.status(201).json({
-                    success: true,
-                    message: 'Registration Was Successful',
-                    data: user
+                // Tạo vai trò 3 (client)
+                Rol.create(user.id, 3, async (err, data) => {
+                    if (err) {
+                        return res.status(501).json({
+                            success: false,
+                            message: 'There Was An Error With User Role Registration',
+                            error: err
+                        });
+                    }
+
+                    return res.status(201).json({
+                        success: true,
+                        message: 'Registration Was Successful',
+                        data: user
+                    });
                 });
-
             });
-
-
         });
-
     },
     async updateWithImage(req, res) {
 
